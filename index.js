68685aa95be5af9aed73015613621e25b7be6f57
Shubham Gaur <98314557+ShubhamGaur-277@users.noreply.github.com>
2023-02-25 16:57:37 +0530

    added new functionality

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
-console.log(randomNumber1);
+document.querySelector("button").addEventListener("click", function () {
+    var randomNumber1 = Math.random();
+    randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+    console.log(randomNumber1);
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
-
-
-var imgList = document.querySelectorAll("img");
+    var randomNumber2 = Math.random();
+    randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
 
 
-imgList[0].setAttribute("src","images/dice"+randomNumber1+".png");
-imgList[1].setAttribute("src","images/dice"+randomNumber2+".png");
+    var imgList = document.querySelectorAll("img");
 
-if(randomNumber1 === randomNumber2){
-    document.querySelector("h1").innerHTML = "Draw";
-}
-else if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩Player 1 wins!!";
-}
 
-else {
-    document.querySelector("h1").innerHTML = "🚩Player 2 wins!!";
-}
+    imgList[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    imgList[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 === randomNumber2) {
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+    else if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩Player 1 wins!!";
+    }
+
+    else {
+        document.querySelector("h1").innerHTML = "🚩Player 2 wins!!";
+    }
+})
